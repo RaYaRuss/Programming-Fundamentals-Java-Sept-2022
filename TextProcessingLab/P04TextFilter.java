c01332264f6d85fe0ef25e3f46856d51819e97f1
Radina Yaneva <radina_yaneva@yahoo.com>
2022-12-15 17:22:02 +0200

    Text Processing Tasks Solutions

---
+++ TextProcessingLab/P04TextFilter.java
@@ -0,0 +1,25 @@
+package TextProcessingLab;
+
+import java.util.Scanner;
+
+public class P04TextFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] banWordsArr = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
+
+        for (String banWord : banWordsArr) {
+            text = text.replace(banWord, repeatString("*", banWord.length()));
+        }
+
+        System.out.println(text);
+    }
+    public static String repeatString (String s, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
